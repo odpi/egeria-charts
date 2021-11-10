eced5e444871fdca1a63c337b7939cd24226f692
Christopher Grote <chris@thegrotes.net>
2021-11-10 14:06:05 +0000

    #87 initial work towards initialization and reporting
    Signed-off-by: Christopher Grote <chris@thegrotes.net>

--- charts/egeria-pts/scripts/collect-results.sh
+++ charts/egeria-pts/scripts/collect-results.sh
@@ -8,22 +8,22 @@ echo -e '\n-- Collecting results of the performance test suite...'
 echo -e '\n > Collecting basic configuration information...'
 
 curl -f -k --silent -X GET \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/configuration" > /tmp/omag.server.${EGERIA_SERVER}.config
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/configuration" > /export/omag.server.${EGERIA_SERVER}.config
 
 curl -f -k --silent -X GET \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/configuration" > /tmp/omag.server.${TUT_SERVER}.config
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/configuration" > /export/omag.server.${TUT_SERVER}.config
 
 curl -f -k --silent -X GET \
-  "${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/local-registration" > /tmp/cohort.${EGERIA_COHORT}.${EGERIA_SERVER}.local
+  "${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/local-registration" > /export/cohort.${EGERIA_COHORT}.${EGERIA_SERVER}.local
 
 curl -f -k --silent -X GET \
-  "${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/cohorts/${EGERIA_COHORT}/remote-members" > /tmp/cohort.${EGERIA_COHORT}.${EGERIA_SERVER}.remote
+  "${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/cohorts/${EGERIA_COHORT}/remote-members" > /export/cohort.${EGERIA_COHORT}.${EGERIA_SERVER}.remote
 
 curl -f -k --silent -X GET \
-  "${EGERIA_ENDPOINT}/servers/${TUT_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/local-registration" > /tmp/cohort.${EGERIA_COHORT}.${TUT_SERVER}.local
+  "${EGERIA_ENDPOINT}/servers/${TUT_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/local-registration" > /export/cohort.${EGERIA_COHORT}.${TUT_SERVER}.local
 
 curl -f -k --silent -X GET \
-  "${EGERIA_ENDPOINT}/servers/${TUT_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/cohorts/${EGERIA_COHORT}/remote-members" > /tmp/cohort.${EGERIA_COHORT}.${TUT_SERVER}.remote
+  "${EGERIA_ENDPOINT}/servers/${TUT_SERVER}/open-metadata/repository-services/users/${EGERIA_USER}/metadata-highway/cohorts/${EGERIA_COHORT}/remote-members" > /export/cohort.${EGERIA_COHORT}.${TUT_SERVER}.remote
 
 echo -e ' > Waiting for the performance test suite to complete...'
 
@@ -36,35 +36,35 @@ until [ $(curl -f -k --silent -X GET ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}
   sleep 30;
 done
 
-curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/summary > /tmp/openmetadata_pts_summary.json
+curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/summary > /export/openmetadata_pts_summary.json
 
 TEST_CASES=$(curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/test-cases  | jq -r '.testCaseIds[]')
 
 PROFILES=$(curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/profiles | jq -r '.profileNames[]')
 
 echo -e '\n > Retrieving detailed profile results...\n'
-mkdir -p /tmp/profile-details
+mkdir -p /export/profile-details
 while read -r line; do
   urlencoded=$(echo ${line} | sed -e 's/ /%20/g');
   filename=$(echo ${line} | sed -e 's/ /_/g');
   echo "   ... retrieving profile details for: ${line}";
-  curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/profiles/${urlencoded} > /tmp/profile-details/${filename}.json;
+  curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/profiles/${urlencoded} > /export/profile-details/${filename}.json;
 done < <(echo "${PROFILES}")
 
 echo -e '\n > Retrieving detailed test case results...\n'
-mkdir -p /tmp/test-case-details
+mkdir -p /export/test-case-details
 while read -r line; do
   urlencoded=$(echo ${line} | sed -e 's/</%3C/g');
   urlencoded=$(echo ${urlencoded} | sed -e 's/>/%3E/g');
   filename=$(echo ${line} | sed -e 's/[<>]/_/g');
   echo "   ... retrieving test case details for: ${line}";
-  curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/test-cases/${urlencoded} > /tmp/test-case-details/${filename}.json;
+  curl -f -k --silent -X GET --max-time 120 ${EGERIA_ENDPOINT}/servers/${EGERIA_SERVER}/open-metadata/conformance-suite/users/${EGERIA_USER}/report/test-cases/${urlencoded} > /export/test-case-details/${filename}.json;
 done < <(echo "${TEST_CASES}")
 
 echo -e '\n > Bundling all results into an archive...\n'
-cd /tmp
+cd /export
 tar cf pd.tar profile-details/*.json; gzip pd.tar
 tar cf tcd.tar test-case-details/*.json; gzip tcd.tar
 tar cf ${PTS_REPORT_NAME}.tar *.config cohort.* openmetadata_pts_summary.json pd.tar.gz tcd.tar.gz; gzip ${PTS_REPORT_NAME}.tar
 
-echo -e "\n-- End of performance test suite results collection, download from: /tmp/${PTS_REPORT_NAME}.tar.gz"
+echo -e "\n-- End of performance test suite results collection, download from: /export/${PTS_REPORT_NAME}.tar.gz"
--- charts/egeria-pts/scripts/config-egeria.sh
+++ charts/egeria-pts/scripts/config-egeria.sh
@@ -12,44 +12,46 @@ echo -e '\n-- Configuring platform with required servers...'
 echo -e '\n > Configuring performance test suite driver:\n'
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}" || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-type?typeName=Conformance"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/server-type?typeName=Conformance" || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
   --header "Content-Type: application/json" \
   "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/event-bus?topicURLRoot=egeria" \
-  --data '{"producer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"}, "consumer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"} }'
+  --data '{"producer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"}, "consumer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"} }' || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/cohorts/${EGERIA_COHORT}"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/cohorts/${EGERIA_COHORT}" || exit $?
+
+echo '{"class":"RepositoryPerformanceWorkbenchConfig","tutRepositoryServerName":"'"${TUT_SERVER}"'","instancesPerType":'${INSTANCES_PER_TYPE}',"maxSearchResults":'${MAX_SEARCH_RESULTS}',"waitBetweenScenarios":'${WAIT_BETWEEN_SCENARIOS}',"profilesToSkip":'${PROFILES_TO_SKIP}',"methodsToSkip":'${METHODS_TO_SKIP}' }' > /tmp/config.json
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
   --header "Content-Type: application/json" \
   "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${EGERIA_SERVER}/conformance-suite-workbenches/repository-workbench/performance" \
-  --data '{"class":"RepositoryPerformanceWorkbenchConfig","tutRepositoryServerName":"'"${TUT_SERVER}"'","instancesPerType":'${INSTANCES_PER_TYPE}',"maxSearchResults":'${MAX_SEARCH_RESULTS}',"waitBetweenScenarios":'${WAIT_BETWEEN_SCENARIOS}',"profilesToSkip":'${PROFILES_TO_SKIP}',"methodsToSkip":'${METHODS_TO_SKIP}' }'
+  --data @/tmp/config.json || exit $?
 
 echo -e '\n > Configuring technology under test:\n'
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/server-url-root?url=${EGERIA_ENDPOINT}" || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/server-type?typeName=TUT"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/server-type?typeName=TUT" || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/organization-name?name=Egeria"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/organization-name?name=Egeria" || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
   --header "Content-Type: application/json" \
   "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/event-bus?topicURLRoot=egeria" \
-  --data '{"producer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"}, "consumer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"} }'
+  --data '{"producer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"}, "consumer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"} }' || exit $?
 
 if [ "${TUT_TYPE}" = "native" ]; then
   if [ "${CONNECTOR_PROVIDER}" = "org.odpi.openmetadata.adapters.repositoryservices.graphrepository.repositoryconnector.GraphOMRSRepositoryConnectorProvider" ]; then
     curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-      "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/local-repository/mode/local-graph-repository"
+      "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/local-repository/mode/local-graph-repository" || exit $?
   else
     echo "-- Unknown native repository provider: ${CONNECTOR_PROVIDER} -- exiting."
     exit 1
@@ -58,18 +60,18 @@ elif [ "${TUT_TYPE}" = "plugin" ]; then
   curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
     --header "Content-Type: application/json" \
     "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/local-repository/mode/plugin-repository/connection" \
-    --data '{"class":"Connection","connectorType":{"class":"ConnectorType","connectorProviderClassName":"'"${CONNECTOR_PROVIDER}"'"},"configurationProperties":'${CONNECTOR_CONFIG}'}}'
+    --data '{"class":"Connection","connectorType":{"class":"ConnectorType","connectorProviderClassName":"'"${CONNECTOR_PROVIDER}"'"},"configurationProperties":'${CONNECTOR_CONFIG}'}}' || exit $?
 elif [ "${TUT_TYPE}" = "proxy" ]; then
   curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
     --header "Content-Type: application/json" \
     "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/local-repository/mode/repository-proxy/connection" \
-    --data '{"class":"Connection","connectorType":{"class":"ConnectorType","connectorProviderClassName":"'"${CONNECTOR_PROVIDER}"'"},"endpoint":{"class":"Endpoint","address":"'"${TUT_HOST}:${TUT_PORT}"'","protocol":"'"${TUT_PROTOCOL}"'"},"userId":"'"${TUT_USER}"'","clearPassword":"'"${TUT_PASS}"'","configurationProperties":'${CONNECTOR_CONFIG}'}}'
+    --data '{"class":"Connection","connectorType":{"class":"ConnectorType","connectorProviderClassName":"'"${CONNECTOR_PROVIDER}"'"},"endpoint":{"class":"Endpoint","address":"'"${TUT_HOST}:${TUT_PORT}"'","protocol":"'"${TUT_PROTOCOL}"'"},"userId":"'"${TUT_USER}"'","clearPassword":"'"${TUT_PASS}"'","configurationProperties":'${CONNECTOR_CONFIG}'}}' || exit $?
 else
   echo "-- Unknown repository type: ${TUT_TYPE} -- exiting."
   exit 1
 fi
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
-  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/cohorts/${EGERIA_COHORT}"
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/cohorts/${EGERIA_COHORT}" || exit $?
 
 echo -e "\n-- End of configuration"
