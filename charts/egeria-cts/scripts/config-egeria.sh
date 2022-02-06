23647ebb5cb2c25709de6b0adc2bbcda0bed54ce
Christopher Grote <cmgrote@users.noreply.github.com>
2022-02-06 00:49:51 +0000

    #121 resolves LOADER_PATH clobbering
    Signed-off-by: Christopher Grote <cmgrote@users.noreply.github.com>

--- charts/egeria-cts/scripts/config-egeria.sh
+++ charts/egeria-cts/scripts/config-egeria.sh
@@ -42,6 +42,9 @@ curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
   "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/organization-name?name=Egeria" || exit $?
 
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/local-repository/metadata-collection-name/TUT_MDR" || exit $?
+
+curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
   --header "Content-Type: application/json" \
   "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/event-bus?topicURLRoot=egeria" \
   --data '{"producer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"}, "consumer": {"bootstrap.servers": "'"${KAFKA_ENDPOINT}"'"} }' || exit $?
