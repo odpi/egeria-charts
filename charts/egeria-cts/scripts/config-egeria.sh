9f172e0a10f95af10eded6f261031b804b14649d
Ljupcho Palashevski <lpalashevski@gmail.com>
2023-07-17 17:43:31 +0200

    Add custom audit-log configuration for TUT server
    Signed-off-by: Ljupcho Palashevski <lpalashevski@gmail.com>

--- charts/egeria-cts/scripts/config-egeria.sh
+++ charts/egeria-cts/scripts/config-egeria.sh
@@ -78,4 +78,16 @@ curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
 curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
   "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/cohorts/${EGERIA_COHORT}" || exit $?
 
+# Custom audit log configuration to help troubleshooting errors and exceptions for TUT server #
+# Remove all audit log destinations (removes default) #
+curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X DELETE \
+  --header "Content-Type: application/json" \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/audit-log-destinations" \
+  --data ''|| exit $?
+# Add the custom console destination using only Error and Exception severities #
+curl -f -k -w "\n   (%{http_code} - %{url_effective})\n" --silent -X POST \
+  --header "Content-Type: application/json" \
+  "${EGERIA_ENDPOINT}/open-metadata/admin-services/users/${EGERIA_USER}/servers/${TUT_SERVER}/audit-log-destinations/console" \
+  --data '["Error","Exception"]'|| exit $?
+
 echo -e "\n-- End of configuration"
